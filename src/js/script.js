26f7f8107d8b553b4f0913ba677777457447005d
Mikhail <potaomg@gmail.com>
2023-01-14 16:45:22 +0300

    feat: modal

--- src/js/script.js
+++ src/js/script.js
@@ -65,3 +65,23 @@ const swiper = new Swiper('.carousel__inner', {
      toggleSlide('.catalog-item__back');
    });
 })(jQuery);
+
+// model
+
+$(document).ready(function(){
+   $('[data-model="consultation"]').on('click', function(){
+      $('.overlay, #consultation').fadeIn('slow');
+   });
+   $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #thanks, #older').fadeOut('slow');
+   });
+
+   $('.button_mini').each(function(i){
+      $(this).on('click', function(){
+         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+         $('.overlay, #order').fadeIn('slow');
+      });
+   });
+});
+
+
